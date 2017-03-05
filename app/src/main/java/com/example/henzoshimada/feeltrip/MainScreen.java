3bc0f95b8e040029f33af37a1aa2dd40ab9f5dac
Hk-tang <hktang@ualberta.ca>
2017-03-05 09:48:11 -0700

    initial project

---
+++ app/src/main/java/com/example/henzoshimada/feeltrip/MainScreen.java
@@ -0,0 +1,14 @@
+package com.example.henzoshimada.feeltrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// This is the main screen: This is what the Participant first sees
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
