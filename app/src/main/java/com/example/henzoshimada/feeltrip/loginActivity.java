b02a98c9e1c9a20ac62dc4b308690734ab02932f
Brett Baker <wbaker@ualberta.ca>
2017-03-20 13:18:30 -0600

    Add Login screen functionality

--- app/src/main/java/com/example/henzoshimada/feeltrip/loginActivity.java
+++ app/src/main/java/com/example/henzoshimada/feeltrip/loginActivity.java
@@ -33,18 +33,44 @@ public class loginActivity extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        if(users.get(0)!=null) {
+        if(!users.isEmpty()) {
             Intent intent = new Intent(this, MainScreen.class);
             startActivity(intent);
         }
-
+        else {
+            Toast.makeText(getApplicationContext(),"Invalid username or password!",Toast.LENGTH_SHORT).show();
+        }
+        return;
     }
     public void regUser(View v){
         EditText userField = (EditText) this.findViewById(R.id.user_text);
         EditText passField = (EditText) this.findViewById(R.id.pass_text);
-        ElasticSearchController.AddUserTask addUserTask = new ElasticSearchController.AddUserTask();
-        User user = new User(userField.getText().toString(), passField.getText().toString());
-        addUserTask.execute(user);
-        Toast.makeText(getApplicationContext(),"User Creation sucessful!",Toast.LENGTH_SHORT).show();
+        ArrayList<User> users = new ArrayList<>();
+        ElasticSearchController.GetUsernameTask get = new ElasticSearchController.GetUsernameTask(userField.getText().toString());
+        get.execute();
+        try {
+            users.addAll(get.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        if(users.isEmpty()) {
+            String userString = userField.getText().toString();
+            String passString = passField.getText().toString();
+            if(!passString.isEmpty() && !userString.isEmpty()) {
+                ElasticSearchController.AddUserTask addUserTask = new ElasticSearchController.AddUserTask();
+                User user = new User(userString, passString);
+                addUserTask.execute(user);
+                Toast.makeText(getApplicationContext(),"User Creation sucessful!",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"Both username and password must be filled!",Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"This user already exists!",Toast.LENGTH_SHORT).show();
+        }
+        return;
     }
 }
