48b1e1c0118b461210911923ef8e0cd80d5a0fef
Ivan <ima@ualberta.ca>
2017-03-21 01:54:03 -0600

    Added filters on the top of main

--- app/src/main/java/com/example/henzoshimada/feeltrip/MainScreen.java
+++ app/src/main/java/com/example/henzoshimada/feeltrip/MainScreen.java
@@ -11,10 +11,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import layout.homeFragmment;
 import layout.mapFragment;
@@ -24,6 +31,43 @@ import layout.profileFragment;
 public class MainScreen extends AppCompatActivity {
     private Context context;
     private Spinner filterSpinner;
+    private String emotionalState;
+    private String socialSit;
+    private Spinner emotionalStateSpinner;
+
+    // The following are constants for emotion based emoji
+    public static final int angry = 0x1F624;
+    public static final int confused = 0x1F635;
+    public static final int disgusted = 0x1F623; //might change this one...
+    public static final int fearful = 0x1F628;
+    public static final int happy = 0x1F60A;
+    public static final int sad = 0x1F622;
+    public static final int shameful = 0x1F61E;
+    public static final int cool = 0x1F60E;
+    public static final int somethingwentwrong = 0x1F31A;
+
+    public int emojiUnicode(String emotion) {
+        switch(emotion) {
+            case "Angry":
+                return angry;
+            case "Confused":
+                return confused;
+            case "Disgusted":
+                return disgusted;
+            case "Fearful":
+                return fearful;
+            case "Happy":
+                return happy;
+            case "Sad":
+                return sad;
+            case "Shameful":
+                return shameful;
+            case "Cool":
+                return cool;
+            default:
+                return somethingwentwrong; // the secret "only accessible by hacks" face
+        }
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -78,8 +122,56 @@ public class MainScreen extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         setFirstItemNavigationView();
+        addItemsOnEmotionalStateSpinner();
+
+        ToggleButton toggleRecent = (ToggleButton) findViewById(R.id.toggleRecent);
+        toggleRecent.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton arg0, boolean isChecked) {
+                Toast.makeText(getApplicationContext(),"Toggled filter for recent posts!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ToggleButton toggleFriends = (ToggleButton) findViewById(R.id.toggleFriends);
+        toggleFriends.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton arg0, boolean isChecked) {
+                Toast.makeText(getApplicationContext(),"Toggled filter for friends!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
-/*
+
+    public String getEmojiByUnicode(int unicode){
+        if (unicode == 0) {
+            return "";
+        }
+        return new String(Character.toChars(unicode));
+    }
+
+    private void addItemsOnEmotionalStateSpinner(){
+        emotionalStateSpinner = (Spinner) findViewById(R.id.filterMood);
+        List<String> emotionalStateList = new ArrayList<>();
+        emotionalStateList.add("Angry " + getEmojiByUnicode(emojiUnicode("Angry")));
+        emotionalStateList.add("Confused " + getEmojiByUnicode(emojiUnicode("Confused")));
+        emotionalStateList.add("Disgusted " + getEmojiByUnicode(emojiUnicode("Disgusted")));
+        emotionalStateList.add("Fearful " + getEmojiByUnicode(emojiUnicode("Fearful")));
+        emotionalStateList.add("Happy " + getEmojiByUnicode(emojiUnicode("Happy")));
+        emotionalStateList.add("Sad " + getEmojiByUnicode(emojiUnicode("Sad")));
+        emotionalStateList.add("Shameful " + getEmojiByUnicode(emojiUnicode("Shameful")));
+        emotionalStateList.add("Cool " + getEmojiByUnicode(emojiUnicode("Cool")));
+
+
+
+        ArrayAdapter<String> emotionalStateAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, emotionalStateList);
+        emotionalStateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        emotionalStateSpinner.setAdapter(emotionalStateAdapter);
+    }
+
+        /*
     // Taken from: https://www.mkyong.com/android/android-spinner-drop-down-list-example/
     // On: March 5, 2017 17:03
     public void addItemsOnFilterSpinner() {
