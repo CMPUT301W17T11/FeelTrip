36d9e68cd8a36acab84e5b543a9df8177e2f041e
MichaelSu37 <ysu3@ualberta.ca>
2017-03-26 23:29:53 -0600

    merge

--- app/src/main/java/com/example/henzoshimada/feeltrip/Mood.java
+++ app/src/main/java/com/example/henzoshimada/feeltrip/Mood.java
@@ -1,8 +1,19 @@
 package com.example.henzoshimada.feeltrip;
 
 // Removed unused imports
+
+import android.text.Html;
+import android.text.TextUtils;
+
+import org.w3c.dom.Text;
+
+import android.graphics.Bitmap;
+
+
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 import io.searchbox.annotations.JestId;
 
@@ -33,6 +44,8 @@ public class Mood {
     private static boolean delState; // 1 for delete, 0 for add
     //TODO: Test deleting a mood from ES after popping from local queue
 
+    private int emoji;
+
     @JestId
     private String id;
 
@@ -51,17 +64,18 @@ public class Mood {
     4 ----> isPrivateChanged;
     5 ----> imageChanged;
     6 ----> locationChanged;
+    7 ----> emoji image
     */
 
     private static boolean[] stateVector;
 
 
-    public Mood(){
+    public Mood() {
         username = null;
-        emotionalState = null;
-        description = null;
+        emotionalState = "";
+        description = "";
         date = 0L;
-        socialSit = null;
+        socialSit = "";
         isPrivate = false;
         image = null;
         latitude = null;
@@ -70,14 +84,15 @@ public class Mood {
         delState = false;
         made = null;
         id = null;
+        emoji = 0;
     }
 
 
-    public Mood(String user){
+    public Mood(String user) {
         this.username = user;
-        emotionalState = null;
-        description = null;
-        socialSit = null;
+        emotionalState = "";
+        description = "";
+        socialSit = "";
         trueDate = new Date();
         date = trueDate.getTime();
         isPrivate = false;
@@ -88,34 +103,38 @@ public class Mood {
         delState = false;
         made = null;
         id = null;
+        emoji = 0;
     }
 
     public String getId() {
-        if (id == null){ //mood does not yet exist within the Elasticsearch database
+        if (id == null) { //mood does not yet exist within the Elasticsearch database
             return "-1";
-        }
-        else {//return ID that the mood is stored under in the Elasticsearch database
+        } else {//return ID that the mood is stored under in the Elasticsearch database
             return id;
         }
     }
 
     public void setId(String id) {
-        this.id = id;
+        if (this.id != id) {
+            this.id = id;
+        }
     }
 
     // Evaluate stateVector and check if this mood event is changed.
-    public boolean isChanged(){
-        for (boolean value: stateVector){
-            if(value){return true;}
+    public boolean isChanged() {
+        for (boolean value : stateVector) {
+            if (value) {
+                return true;
+            }
         }
         return false;
     }
 
-    public boolean[] getAllState(){
+    public boolean[] getAllState() {
         return stateVector;
     }
 
-    public void resetState(){
+    public void resetState() {
         Arrays.fill(stateVector, false);
     }
 
@@ -123,16 +142,21 @@ public class Mood {
         return stateVector[index];
     }
 
-    private void setStateByIndex(int index){
-        stateVector[index] = true;
+    public void setStateByIndex(int index) {
+        if (!stateVector[index]) {
+            stateVector[index] = true;
+        }
     }
 
 
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
 
     public String getImage() {
+        if (image != null) {
+            return Html.fromHtml(image).toString();
+        }
         return image;
     }
 
@@ -141,32 +165,42 @@ public class Mood {
     }
 
     public void setEmotionalState(String emotionalState) {
-        this.emotionalState = emotionalState;
-        setStateByIndex(0);
+        if (!getEmotionalState().equals(emotionalState)) {
+            this.emotionalState = emotionalState;
+            setStateByIndex(0);
+        }
     }
 
     public void setImage(String image) {
-        this.image = image;
-        setStateByIndex(5);
+        if (getImage() == null) {
+            this.image = TextUtils.htmlEncode(image);
+            setStateByIndex(5);
+        } else if (!getImage().equals(image)) {
+            this.image = TextUtils.htmlEncode(image);
+            setStateByIndex(5);
+        }
     }
 
     public String getDescription() {
-        return description;
+        return Html.fromHtml(description).toString();
     }
 
-    public void setDescription(String description, String append) throws DescriptionTooLongException {
-        int count = 0;
-        for (int i = 0; i < description.length(); i++){
-            if (description.charAt(i) == ' '){
-                count++;
+    public void setDescription(String descriptionin) throws DescriptionTooLongException {
+        if (!getDescription().equals(descriptionin)) {
+            int count = 0;
+            String description = descriptionin.trim();
+            for (int i = 0; i < description.length(); i++) {
+                if (description.charAt(i) == ' ') {
+                    count++;
+                }
             }
+            if (description.length() > 20 || count > 2) {
+                throw new DescriptionTooLongException();
+            } else {
+                this.description = TextUtils.htmlEncode(description);
+            }
+            setStateByIndex(1);
         }
-        if (description.length() > 20 || count > 2){
-            throw new DescriptionTooLongException();
-        }else {
-            this.description = description + append;
-        }
-        setStateByIndex(1);
     }
 
     public String getSocialSit() {
@@ -174,18 +208,27 @@ public class Mood {
     }
 
     public void setSocialSit(String socialSit) {
-        this.socialSit = socialSit;
-        setStateByIndex(3);
+        if (!getSocialSit().equals(socialSit)) {
+            this.socialSit = socialSit;
+            setStateByIndex(3);
+        }
     }
 
-    public void setMapPosition(Double latitude, Double longitude){
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.location = latitude + ", " + longitude;
-        setStateByIndex(6);
+    public void setMapPosition(Double latitude, Double longitude) {
+        if (this.location == null) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.location = latitude + ", " + longitude;
+            setStateByIndex(6);
+        } else if (!getLocation().equals(latitude + ", " + longitude)) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.location = latitude + ", " + longitude;
+            setStateByIndex(6);
+        }
     }
 
-    public Double[] getMapPosition(){
+    public Double[] getMapPosition() {
         Double[] loc = new Double[2];
         loc[0] = this.latitude;
         loc[1] = this.longitude;
@@ -197,13 +240,17 @@ public class Mood {
     }
 
     public void setPrivate() {
-        isPrivate = true;
-        setStateByIndex(4);
+        if (!this.isPrivate) {
+            isPrivate = true;
+            setStateByIndex(4);
+        }
     }
 
     public void setPublic() {
-        isPrivate = false;
-        setStateByIndex(4);
+        if (this.isPrivate) {
+            isPrivate = false;
+            setStateByIndex(4);
+        }
     }
 
     public String getMoodOption() {
@@ -211,8 +258,10 @@ public class Mood {
     }
 
     public void setMoodOption(String emotionalState) {
-        this.emotionalState = emotionalState;
-        setStateByIndex(0);
+        if (!getEmotionalState().equals(emotionalState)) {
+            this.emotionalState = emotionalState;
+            setStateByIndex(0);
+        }
     }
 
     public Date getDate() {
@@ -221,9 +270,11 @@ public class Mood {
     }
 
     public void setDate(Date date) {
-        this.date = date.getTime();
-        this.trueDate = date;
-        setStateByIndex(2);
+        if (this.date != date.getTime()) {
+            this.date = date.getTime();
+            this.trueDate = date;
+            setStateByIndex(2);
+        }
     }
 
     public void setDel() {
@@ -239,23 +290,29 @@ public class Mood {
     }
 
     public Double getLatitude() {
-        return latitude;
+        List<String> loclist = Arrays.asList(location.split(","));
+        return Double.parseDouble(loclist.get(0).trim());
     }
 
     public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-        this.location = latitude + ", " + longitude;
-        setStateByIndex(6);
+        if (getLatitude() != latitude) {
+            this.latitude = latitude;
+            this.location = latitude + ", " + longitude;
+            setStateByIndex(6);
+        }
     }
 
     public Double getLongitude() {
-        return longitude;
+        List<String> loclist = Arrays.asList(location.split(","));
+        return Double.parseDouble(loclist.get(1).trim());
     }
 
     public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-        this.location = latitude + ", " + longitude;
-        setStateByIndex(6);
+        if (getLongitude() != longitude) {
+            this.longitude = longitude;
+            this.location = latitude + ", " + longitude;
+            setStateByIndex(6);
+        }
     }
 
     public Long getMade() {
@@ -268,6 +325,31 @@ public class Mood {
 
     public String getLocation() {
         return location;
+    }
+
+    public void setNullImage() {
+        if (this.image != null) {
+            this.image = null;
+            setStateByIndex(5);
+        }
+    }
+
+    public void setNullLocation() {
+        if (this.location != null) {
+            this.location = null;
+            setStateByIndex(6);
+        }
+    }
+
+    public int getEmoji() {
+        return emoji;
+    }
+
+    public void setEmoji(int emoji) {
+        if(this.emoji != emoji) {
+            this.emoji = emoji;
+            setStateByIndex(7);
+        }
     }
 
 }
